ce1cad230d71624a3d10a1c14c63a70d941e5ca0
Miles di Prata <milesdiprata3@gmail.com>
2022-12-26 20:55:13 -0800

    Day5: Completed part one.

---
+++ src/day5.rs
@@ -0,0 +1,143 @@
+use anyhow::{anyhow, Result};
+use std::io::{self, BufRead};
+
+#[derive(Debug)]
+struct Crate(char);
+
+#[derive(Debug)]
+struct Stack(Vec<Crate>);
+
+#[derive(Debug)]
+struct Crane;
+
+#[derive(Debug)]
+struct Rearrangement {
+    stack_len: usize,
+    source: usize,
+    destination: usize,
+}
+
+fn read_stacks() -> Result<Vec<Stack>> {
+    let mut lines = io::stdin().lock().lines();
+    let mut rows = Vec::new();
+
+    while let Some(Ok(line)) = lines.next() {
+        if line.is_empty() {
+            break;
+        }
+
+        let mut row = Vec::new();
+
+        line.char_indices().for_each(|(idx, char)| {
+            if ('A'..='Z').contains(&char) {
+                row.push(Some(char));
+            } else if idx % 2 != 0 && (idx + 1) % 4 != 0 {
+                row.push(None);
+            }
+        });
+
+        if row.iter().any(|char| char.is_some()) {
+            rows.push(row);
+        }
+    }
+
+    let stacks = (0..rows
+        .first()
+        .ok_or_else(|| anyhow!("No stacks given!"))?
+        .len())
+        .rev()
+        .map(|i| {
+            rows.iter()
+                .rev()
+                .filter_map(|inner| inner[i])
+                .map(Crate)
+                .collect()
+        })
+        .rev()
+        .map(Stack)
+        .collect();
+
+    Ok(stacks)
+}
+
+fn read_rearrangements() -> Result<Vec<Rearrangement>> {
+    let mut lines = io::stdin().lock().lines();
+    let mut rearrangements = Vec::new();
+
+    while let Some(Ok(line)) = lines.next() {
+        if line.is_empty() {
+            break;
+        }
+
+        let mut split = line.split_whitespace().flat_map(|str| str.parse::<usize>());
+
+        let stack_len = split
+            .next()
+            .ok_or_else(|| anyhow!("No stack length given!"))?;
+
+        let source = split
+            .next()
+            .ok_or_else(|| anyhow!("No source stack given!"))?;
+
+        let destination = split
+            .next()
+            .ok_or_else(|| anyhow!("No destination stack given!"))?;
+
+        rearrangements.push(Rearrangement {
+            stack_len,
+            source,
+            destination,
+        })
+    }
+
+    Ok(rearrangements)
+}
+
+impl Crane {
+    fn top_crates(stacks: &[Stack]) -> Option<String> {
+        stacks
+            .iter()
+            .map(|stack| stack.0.last())
+            .map(|item| item.map(|item| item.0))
+            .collect()
+    }
+
+    fn rearrange(stacks: &mut [Stack], rearrangement: &Rearrangement) -> Option<()> {
+        (0..rearrangement.stack_len)
+            .map(|_| {
+                stacks
+                    .get_mut(rearrangement.source - 1)
+                    .map(|stack| stack.0.pop())
+            })
+            .collect::<Option<Option<Vec<_>>>>()??
+            .into_iter()
+            .map(|item| {
+                stacks
+                    .get_mut(rearrangement.destination - 1)
+                    .map(|stack| stack.0.push(item))
+            })
+            .collect()
+    }
+}
+
+fn part_one(stacks: &mut [Stack], rearrangements: &[Rearrangement]) -> Option<String> {
+    rearrangements
+        .iter()
+        .map(|rearrangement| Crane::rearrange(stacks, rearrangement))
+        .collect::<Option<_>>()?;
+
+    Crane::top_crates(stacks)
+}
+
+fn main() -> Result<()> {
+    let mut stacks = read_stacks()?;
+    let rearrangements = read_rearrangements()?;
+
+    println!(
+        "Part one: {}",
+        part_one(stacks.as_mut_slice(), rearrangements.as_slice())
+            .ok_or_else(|| anyhow!("No stacks given!"))?
+    );
+
+    Ok(())
+}
