0e771c0d3ebec27656cfe6236767218494501b39
Miles di Prata <milesdiprata3@gmail.com>
2023-01-02 09:32:28 -0800

    Day9: Completed part two.

--- src/day9.rs
+++ src/day9.rs
@@ -6,13 +6,14 @@ use anyhow::Result;
 use day9::{Motion, Rope};
 
 mod day9 {
+    use std::collections::VecDeque;
     use std::io::{BufRead, Stdin};
     use std::str::FromStr;
 
     use anyhow::{anyhow, Error, Result};
 
     #[derive(Clone, Copy, Debug)]
-    pub enum Direction {
+    enum Direction {
         Up,
         Left,
         Right,
@@ -22,14 +23,14 @@ mod day9 {
     #[derive(Debug)]
     pub struct Motion {
         dir: Direction,
-        steps: isize,
+        len: usize,
     }
 
-    #[derive(Debug, Default)]
-    pub struct Rope {
-        head: (isize, isize),
-        tail: (isize, isize),
-    }
+    #[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
+    pub struct Knot(isize, isize);
+
+    #[derive(Debug)]
+    pub struct Rope(VecDeque<Knot>);
 
     impl FromStr for Direction {
         type Err = Error;
@@ -58,12 +59,12 @@ mod day9 {
                 .next()
                 .map(|dir| dir.parse())
                 .ok_or_else(|| anyhow!("Missing direction!"))??;
-            let steps = split
+            let len = split
                 .next()
-                .map(isize::from_str)
-                .ok_or_else(|| anyhow!("Missing steps!"))??;
+                .map(usize::from_str)
+                .ok_or_else(|| anyhow!("Missing len!"))??;
 
-            Ok(Self { dir, steps })
+            Ok(Self { dir, len })
         }
     }
 
@@ -84,67 +85,119 @@ mod day9 {
         }
     }
 
-    impl Rope {
-        pub fn step(&mut self, motion: &Motion) -> Vec<(isize, isize)> {
-            (0..motion.steps)
-                .map(|_| self.sub_step(motion.dir))
-                .collect()
-        }
-
-        fn sub_step(&mut self, dir: Direction) -> (isize, isize) {
-            self.head = Self::straight_sub_step(self.head, dir);
-
-            if self.head.0.abs_diff(self.tail.0) > 1 || self.head.1.abs_diff(self.tail.1) > 1 {
-                self.tail = match self.head.0 == self.tail.0 || self.head.1 == self.tail.1 {
-                    true => Self::straight_sub_step(self.tail, dir),
-                    _ => Self::diagonal_sub_step(self.tail, self.head),
-                };
-            }
-
-            self.tail
-        }
-
-        fn straight_sub_step((i, j): (isize, isize), dir: Direction) -> (isize, isize) {
+    impl Knot {
+        fn move_head(&mut self, dir: Direction) {
             match dir {
-                Direction::Up => (i + 1, j),
-                Direction::Left => (i, j - 1),
-                Direction::Right => (i, j + 1),
-                Direction::Down => (i - 1, j),
+                Direction::Up => self.0 += 1,
+                Direction::Left => self.1 -= 1,
+                Direction::Right => self.1 += 1,
+                Direction::Down => self.0 -= 1,
             }
         }
 
-        fn diagonal_sub_step(
-            (i, j): (isize, isize),
-            (i_target, j_target): (isize, isize),
-        ) -> (isize, isize) {
-            let i = match i_target > i {
-                true => i + 1,
-                _ => i - 1,
+        fn follow(&mut self, head: &Self) {
+            let i_diff = head.0.abs_diff(self.0);
+            let j_diff = head.1.abs_diff(self.1);
+
+            if (i_diff == 2 && j_diff == 0) || (i_diff == 0 && j_diff == 2) {
+                self.step(head);
+            } else if i_diff > 1 || j_diff > 1 {
+                self.diagonal_step(head);
+            }
+        }
+
+        fn step(&mut self, head: &Self) {
+            if head.0 > self.0 {
+                self.0 += 1;
+            } else if self.0 > head.0 {
+                self.0 -= 1;
+            } else if head.1 > self.1 {
+                self.1 += 1;
+            } else if self.1 > head.1 {
+                self.1 -= 1;
+            }
+        }
+
+        fn diagonal_step(&mut self, head: &Self) {
+            match head.0 > self.0 {
+                true => self.0 += 1,
+                _ => self.0 -= 1,
             };
 
-            let j = match j_target > j {
-                true => j + 1,
-                false => j - 1,
+            match head.1 > self.1 {
+                true => self.1 += 1,
+                _ => self.1 -= 1,
             };
+        }
+    }
 
-            (i, j)
+    impl Rope {
+        pub fn with_knots(knots: usize) -> Result<Self> {
+            match knots {
+                (2..) => Ok(Self((0..knots).map(|_| Knot::default()).collect())),
+                _ => Err(anyhow!("Minimum of two knots required!")),
+            }
+        }
+
+        pub fn step(&mut self, motion: &Motion) -> Option<Vec<Knot>> {
+            let knots_len = self.0.len();
+            let mut tails = Vec::with_capacity(motion.len);
+
+            (0..motion.len)
+                .map(|_| {
+                    self.0.front_mut()?.move_head(motion.dir);
+
+                    (1..knots_len)
+                        .map(|i| {
+                            let head = self.0.get(i - 1).cloned()?;
+                            let tail = self.0.get_mut(i)?;
+
+                            tail.follow(&head);
+
+                            Some(())
+                        })
+                        .collect::<Option<_>>()?;
+
+                    tails.push(self.0.back().cloned()?);
+
+                    Some(())
+                })
+                .collect::<Option<_>>()?;
+
+            Some(tails)
         }
     }
 }
 
-fn part_one(rope: &mut Rope, motions: &[Motion]) -> usize {
+fn count_tails(mut rope: Rope, motions: &[Motion]) -> usize {
     motions
         .iter()
         .flat_map(|motion| rope.step(motion))
+        .flatten()
         .collect::<HashSet<_>>()
         .len()
 }
 
+fn part_one(rope: Rope, motions: &[Motion]) -> usize {
+    count_tails(rope, motions)
+}
+
+fn part_two(rope: Rope, motions: &[Motion]) -> usize {
+    count_tails(rope, motions)
+}
+
 fn main() -> Result<()> {
     let motions = Motion::from_stdin(io::stdin())?;
-    let mut rope = Rope::default();
 
-    println!("Part one: {}", part_one(&mut rope, motions.as_slice()));
+    println!(
+        "Part one: {}",
+        part_one(Rope::with_knots(2)?, motions.as_slice())
+    );
+
+    println!(
+        "Part two: {}",
+        part_two(Rope::with_knots(10)?, motions.as_slice())
+    );
 
     Ok(())
 }
