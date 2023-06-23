c6f6e44b820226a528d675ebac2a6a52758c68ef
Miles di Prata <milesdiprata3@gmail.com>
2023-06-23 11:54:35 -0700

    Day10: Completed part two.

--- src/day10.rs
+++ src/day10.rs
@@ -20,16 +20,25 @@ mod day10 {
 
     pub struct Cpu {
         clk: Clock,
+        crt: Crt,
         x: isize,
     }
 
     #[derive(Default)]
-    struct Clock(usize);
+    struct Clock {
+        cycle: usize,
+    }
+
+    #[derive(Default)]
+    struct Crt {
+        x: u8,
+    }
 
     impl Default for Cpu {
         fn default() -> Self {
             Self {
                 clk: Default::default(),
+                crt: Default::default(),
                 x: 1,
             }
         }
@@ -62,7 +71,12 @@ mod day10 {
             stdin
                 .lock()
                 .lines()
-                .take_while(|line| line.is_ok() && !line.as_ref().unwrap().is_empty())
+                .take_while(|line| {
+                    line.as_ref()
+                        .map(|line| line.is_empty())
+                        .map(|empty| !empty)
+                        .unwrap_or_default()
+                })
                 .flatten()
                 .map(|line| line.parse())
                 .collect()
@@ -78,12 +92,13 @@ mod day10 {
 
     impl Clock {
         fn tick(&mut self) -> Option<usize> {
-            self.0 += 1;
+            self.cycle += 1;
+            self.interesting_cycle()
+        }
 
-            match self.0 == 20 || (self.0 > 20 && (self.0 - 20) % 40 == 0) {
-                true => Some(self.0),
-                _ => None,
-            }
+        fn interesting_cycle(&self) -> Option<usize> {
+            (self.cycle == 20 || (self.cycle > 20 && (self.cycle - 20) % 40 == 0))
+                .then_some(self.cycle)
         }
     }
 
@@ -103,17 +118,37 @@ mod day10 {
                     sig_strength = Some(cycle as isize * self.x);
                 }
 
+                self.crt.draw(self.x);
+
                 match instr {
-                    Instr::Noop => (),
-                    Instr::AddX(val) => {
-                        if i == 1 {
-                            self.x += val
-                        }
-                    }
+                    Instr::AddX(val) if i == 1 => self.x += val,
+                    _ => (),
                 }
             });
 
             sig_strength
+        }
+    }
+
+    impl Crt {
+        fn draw(&mut self, x: isize) {
+            let pixel = match [x - 1, x, x + 1]
+                .iter()
+                .map(|&x| x as u8)
+                .any(|x| x == self.x)
+            {
+                true => '#',
+                _ => '.',
+            };
+
+            print!("{pixel}");
+
+            self.x += 1;
+            self.x %= 40;
+
+            if self.x == 0 {
+                println!();
+            }
         }
     }
 }
@@ -125,6 +160,7 @@ fn part_one(mut cpu: Cpu, program: &[Instr]) -> isize {
 fn main() -> Result<()> {
     let program = Instr::from_stdin(io::stdin())?;
 
+    println!();
     println!("Part one: {}", part_one(Cpu::default(), program.as_slice()));
 
     Ok(())
